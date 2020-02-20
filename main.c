8984b0a45e041cb8f01be0ed9687f51686d27ac6
Evgeny Cherkashin <eugeneai@irnok.net>
2020-02-20 14:12:41 +0800

    Added structure for tree representation.

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+typedef union tree_s tree_t;  // Forward declaration
+
+typedef struct tree_s {
+  tree_t * l;
+  char * val;
+  tree_t * r;
+};
+
+
 int main(int argc, char *argv[])
 {
   printf("Hello, World!\n %s \n", argv[0]);
