c640bb60d1aff2071ea1d4f17bc848e777a031d2
Evgeny Cherkashin <eugeneai@irnok.net>
2020-02-20 14:20:52 +0800

    Added constructor.

--- main.c
+++ main.c
@@ -9,6 +9,14 @@ typedef struct tree_s {
 };
 
 
+tree_t * new_tree(tree_t * l, char * val, tree_t * r) {
+  tree_t * n; // FIXME: Add memory allocation
+  n->l=l;
+  n->r=r;
+  n->val=val;
+  return n;
+}
+
 int main(int argc, char *argv[])
 {
   printf("Hello, World!\n %s \n", argv[0]);
