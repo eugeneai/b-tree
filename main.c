e10792df05ec86ac8de733bf134bd14769092cce
Evgeny Cherkashin <eugeneai@irnok.net>
2020-02-20 14:28:08 +0800

    Added destructor for tree structure.

--- main.c
+++ main.c
@@ -17,6 +17,13 @@ tree_t * new_tree(tree_t * l, char * val, tree_t * r) {
   return n;
 }
 
+void delete_tree(tree_t * n, int delete_val) {
+  if (delete_val and n->val!=NULL) free(n->val);
+  if (n->l!=NULL) delete_tree(n->l);
+  if (n->r!=NULL) delete_tree(n->r);
+  free(n);
+}
+
 int main(int argc, char *argv[])
 {
   printf("Hello, World!\n %s \n", argv[0]);
